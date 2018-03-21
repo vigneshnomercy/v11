65ea22ee5e08e62cd5b633ef9d82a37d80870dc4
vigneshnomercy <37579861+vigneshnomercy@users.noreply.github.com>
2018-03-21 10:31:24 +0530

    Create set1-2.cpp

---
+++ set1-2.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main(){
+int a;
+printf ("enter your number");
+scanf("%d",&a);
+if(a/2==0)
+{printf (" your number even");
+}
+else
+{printf (" your number odd");
+}getch();
+}
